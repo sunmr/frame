8887b6612a857dbcd27a8adc0cf2bb3579d7d43e
sunmr <154095673@qq.com>
2017-07-18 16:53:17 +0800

    7.18
    7.18 redis for php

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -1,8 +1,31 @@
 <?php 
 	namespace app\admin\controller;
+	// use think\cache\driver\redis;
 	class Index{
 		public function Index(){
 			// return "admin Index index";
-			dump(config());
+			// dump(config());
+			// $redis = new \Redis();
+			// $redis->set('text','hello,world');
+			// echo $redis->get('text');
+
+        $redis = new \Redis();  
+        $ret = $redis->connect("127.0.0.1",6379); 
+        $redis->set("user","xuwenqiang");  
+        // echo $redis->get("user");
+        $redis->set('user','\nhello');
+        // echo $redis->get('user');  
+        $redis ->del('user');
+        var_dump($redis->get('user'));
+        echo '</br>';
+        echo '----------';
+
+        $redis->lpush('list','html');//从（左）头插入
+        $redis->lpush('list','css');
+        $redis->lpush('list','php');
+
+        $redis->rpush('list','js');
+        $list=$redis->lrange('list',0,-1);//从0开始 结束为-1 获取开始之后的全部
+        var_dump($list);
 		}
 	}
